3c4322a5f500ff7b93a56ca04361c5e70ae1570c
apatrida <jayson.minard@gmail.com>
2017-01-03 15:07:11 -0300

    correct test to use GenericRepl.eval (compileAndEval) which appears has a problem

--- kotlin-script-util/src/test/kotlin/org/jetbrains/kotlin/script/util/TestGenericEngine.kt
+++ kotlin-script-util/src/test/kotlin/org/jetbrains/kotlin/script/util/TestGenericEngine.kt
@@ -2,9 +2,9 @@ package org.jetbrains.kotlin.script.util
 
 import com.intellij.openapi.Disposable
 import com.intellij.openapi.util.Disposer
-import org.jetbrains.kotlin.cli.common.CLIConfigurationKeys
 import org.jetbrains.kotlin.cli.common.messages.MessageRenderer
 import org.jetbrains.kotlin.cli.common.messages.PrintingMessageCollector
+import org.jetbrains.kotlin.cli.common.repl.ReplCheckResult
 import org.jetbrains.kotlin.cli.common.repl.ReplCodeLine
 import org.jetbrains.kotlin.cli.common.repl.ReplCompileResult
 import org.jetbrains.kotlin.cli.common.repl.ReplEvalResult
@@ -14,16 +14,16 @@ import org.jetbrains.kotlin.cli.jvm.config.jvmClasspathRoots
 import org.jetbrains.kotlin.cli.jvm.repl.GenericRepl
 import org.jetbrains.kotlin.config.CommonConfigurationKeys
 import org.jetbrains.kotlin.config.CompilerConfiguration
-import org.jetbrains.kotlin.config.JVMConfigurationKeys
 import org.jetbrains.kotlin.script.KotlinScriptDefinition
 import org.jetbrains.kotlin.script.KotlinScriptDefinitionFromAnnotatedTemplate
 import org.jetbrains.kotlin.script.StandardScriptDefinition
 import org.jetbrains.kotlin.script.util.templates.StandardArgsScriptTemplateWithMavenResolving
 import org.jetbrains.kotlin.utils.PathUtil
-import org.junit.Ignore
 import org.junit.Test
+import java.util.concurrent.atomic.AtomicInteger
 import kotlin.reflect.KClass
 import kotlin.script.templates.standard.ScriptTemplateWithArgs
+import kotlin.test.assertEquals
 import kotlin.test.assertTrue
 import kotlin.test.fail
 
@@ -62,14 +62,29 @@ class TestGenericEngine {
         return GenericRepl(disposable, scriptDefinition, configuration, messageCollector, null)
     }
 
-    fun GenericRepl.compileAndEval(line: Int, code: String) {
-        val line1 = ReplCodeLine(line, code)
-        val compileResult = compile(line1, emptyList())
-        if (compileResult is ReplCompileResult.CompiledClasses) {
-            val evalResult = eval(line1, compileResult.updatedHistory.dropLast(1))
-            assertTrue(evalResult is ReplEvalResult.UnitResult || evalResult is ReplEvalResult.ValueResult, "Eval failed $evalResult")
-        } else {
-            fail("Error $compileResult")
+    class QuickScriptDefinition(template: KClass<out Any>): KotlinScriptDefinition(template)
+
+    data class ReplMemory(val lastLine: AtomicInteger = AtomicInteger(0), var history: List<ReplCodeLine> = arrayListOf())
+
+    val replMemory = hashMapOf<GenericRepl, ReplMemory>()
+
+    fun GenericRepl.runCode(code: String): Any? {
+        val memory = replMemory.getOrPut(this) { ReplMemory() }
+        val codeLine = ReplCodeLine(memory.lastLine.incrementAndGet(), code)
+        val evalResult = eval(codeLine, memory.history)
+
+        return when (evalResult) {
+            is ReplEvalResult.Error.CompileTime,
+                    is ReplEvalResult.HistoryMismatch,
+                    is ReplEvalResult.Incomplete -> fail ("Compilation failed $evalResult")
+            is ReplEvalResult.Error.Runtime -> fail("Runtime failed $evalResult")
+            is  ReplEvalResult.UnitResult,
+                    is ReplEvalResult.ValueResult -> {
+                memory.history = evalResult.updatedHistory
+                if (evalResult is ReplEvalResult.ValueResult) evalResult.value
+                else Unit
+            }
+            else -> fail("unknown failure $evalResult")
         }
     }
 
@@ -77,8 +92,11 @@ class TestGenericEngine {
     fun testGenericEngineBasics() {
         val disposable = Disposer.newDisposable()
         try {
-            val repl = makeEngine(disposable, StandardScriptDefinition)
-            repl.compileAndEval(1, "val x = 10")
+            val repl = makeEngine(disposable, QuickScriptDefinition(ScriptTemplateWithArgs::class))
+            repl.runCode("val x = 10")
+            repl.runCode("val y = 33")
+            val result = repl.runCode("x + y")
+            assertEquals(43, result)
         } finally {
             disposable.dispose()
         }
@@ -89,7 +107,7 @@ class TestGenericEngine {
         val disposable = Disposer.newDisposable()
         try {
             val repl = makeEngine(disposable, StandardArgsScriptTemplateWithMavenResolving::class)
-            repl.compileAndEval(1, """
+            repl.runCode("""
                 @file:DependsOn("uy.klutter:klutter-core-jdk8:1.20.1")
                 import uy.klutter.core.jdk8.*
                 println(utcNow().toIsoString())
