5dedd3d5738f00c1e8904ed5ff1e731ee6105366
Alexander Eliseyev <a.a.eliseyev@gmail.com>
2017-10-16 15:07:23 +0700

    Missing methods with same signature (except for method name) fix in FullClassAllowancesGenerator

--- cuarentena/src/main/kotlin/uy/kohesive/cuarentena/kotlin/FullClassAllowancesGenerator.kt
+++ cuarentena/src/main/kotlin/uy/kohesive/cuarentena/kotlin/FullClassAllowancesGenerator.kt
@@ -1,9 +1,12 @@
 package uy.kohesive.cuarentena.kotlin
 
 import uy.kohesive.cuarentena.policy.*
+import java.lang.reflect.Constructor
+import java.lang.reflect.Modifier
+import java.lang.reflect.Modifier.*
 
 fun main(args: Array<String>) {
-    FullClassAllowancesGenerator().generateAllowances(Class.forName("kotlin.Unit")).toPolicy().forEach {
+    FullClassAllowancesGenerator().generateAllowances(Class.forName("kotlin.collections.CollectionsKt")).toPolicy().forEach {
         println(it)
     }
 }
@@ -15,31 +18,35 @@ class FullClassAllowancesGenerator {
 
         // Constructors
         val constructorAllowances = clazz.declaredConstructors
-            .filter { java.lang.reflect.Modifier.isPublic(it.modifiers) }
-            .map { constructor: java.lang.reflect.Constructor<*> ->
+            .filter { isPublic(it.modifiers) }
+            .map { constructor: Constructor<*> ->
                 PolicyAllowance.ClassLevel.ClassConstructorAccess(currentClassName, constructor.signature(), setOf(AccessTypes.call_Class_Constructor))
             }
 
         // Methods
         val methodAllowances = clazz.methods
-            .filter { java.lang.reflect.Modifier.isPublic(it.modifiers) }
+            .filter { isPublic(it.modifiers) }
             .map { method ->
-                val access = if (java.lang.reflect.Modifier.isStatic(method.modifiers)) uy.kohesive.cuarentena.policy.AccessTypes.call_Class_Static_Method
-                else uy.kohesive.cuarentena.policy.AccessTypes.call_Class_Instance_Method
+                val access = if (Modifier.isStatic(method.modifiers)) {
+                    AccessTypes.call_Class_Static_Method
+                } else  {
+                    AccessTypes.call_Class_Instance_Method
+                }
+
                 val signature = method.signature()
 
-                signature to PolicyAllowance.ClassLevel.ClassMethodAccess(currentClassName, method.name, signature, setOf(access))
+                "${method.name}$signature" to PolicyAllowance.ClassLevel.ClassMethodAccess(currentClassName, method.name, signature, setOf(access))
             }.distinctBy { it.first }.map { it.second }
 
         // Fields
         val fieldsAllowances = clazz.fields
-            .filter { java.lang.reflect.Modifier.isPublic(it.modifiers) }
+            .filter { isPublic(it.modifiers) }
             .map { field ->
                 val access = when {
-                    java.lang.reflect.Modifier.isStatic(field.modifiers) && java.lang.reflect.Modifier.isFinal(field.modifiers) -> setOf(uy.kohesive.cuarentena.policy.AccessTypes.read_Class_Static_Field)
-                    java.lang.reflect.Modifier.isStatic(field.modifiers) -> setOf(uy.kohesive.cuarentena.policy.AccessTypes.read_Class_Static_Field, uy.kohesive.cuarentena.policy.AccessTypes.write_Class_Static_Field)
-                    java.lang.reflect.Modifier.isFinal(field.modifiers) -> setOf(uy.kohesive.cuarentena.policy.AccessTypes.read_Class_Instance_Field)
-                    else -> setOf(uy.kohesive.cuarentena.policy.AccessTypes.read_Class_Instance_Field, uy.kohesive.cuarentena.policy.AccessTypes.write_Class_Instance_Field)
+                    isStatic(field.modifiers) && isFinal(field.modifiers) -> setOf(AccessTypes.read_Class_Static_Field)
+                    isStatic(field.modifiers) -> setOf(AccessTypes.read_Class_Static_Field, AccessTypes.write_Class_Static_Field)
+                    isFinal(field.modifiers)  -> setOf(AccessTypes.read_Class_Instance_Field)
+                    else -> setOf(AccessTypes.read_Class_Instance_Field, AccessTypes.write_Class_Instance_Field)
                 }
                 val signature = field.signature()
 
