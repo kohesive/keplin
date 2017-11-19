589626efb2f9b9696291cf77eb87d956e0132ba0
apatrida <jayson.minard@gmail.com>
2017-11-19 14:38:29 -0200

    fix default import handling

--- keplin-core/src/main/kotlin/uy/kohesive/keplin/kotlin/script/resolver/AnnotationTriggeredScriptDefinition.kt
+++ keplin-core/src/main/kotlin/uy/kohesive/keplin/kotlin/script/resolver/AnnotationTriggeredScriptDefinition.kt
@@ -1,32 +1,18 @@
 package uy.kohesive.keplin.kotlin.script.resolver
 
-import org.jetbrains.kotlin.builtins.DefaultBuiltIns
 import org.jetbrains.kotlin.cli.common.repl.ScriptArgsWithTypes
 import org.jetbrains.kotlin.com.intellij.openapi.diagnostic.Logger
-import org.jetbrains.kotlin.com.intellij.openapi.project.Project
-import org.jetbrains.kotlin.com.intellij.openapi.vfs.StandardFileSystems
-import org.jetbrains.kotlin.com.intellij.openapi.vfs.VirtualFile
-import org.jetbrains.kotlin.com.intellij.psi.PsiFile
-import org.jetbrains.kotlin.com.intellij.psi.PsiManager
-import org.jetbrains.kotlin.config.LanguageVersionSettingsImpl
 import org.jetbrains.kotlin.name.Name
 import org.jetbrains.kotlin.name.NameUtils
-import org.jetbrains.kotlin.psi.KtAnnotationEntry
-import org.jetbrains.kotlin.psi.KtFile
 import org.jetbrains.kotlin.psi.KtScript
-import org.jetbrains.kotlin.psi.KtUserType
-import org.jetbrains.kotlin.resolve.BindingTraceContext
-import org.jetbrains.kotlin.resolve.constants.evaluate.ConstantExpressionEvaluator
 import org.jetbrains.kotlin.script.KotlinScriptDefinitionFromAnnotatedTemplate
-import org.jetbrains.kotlin.script.tryCreateCallableMappingFromNamedArgs
-import org.jetbrains.kotlin.types.TypeUtils
 import uy.kohesive.keplin.util.ApiChangeDependencyResolverWrapper
+import uy.kohesive.keplin.util.EmptyDependencies
+import uy.kohesive.keplin.util.EmptyDependenciesWithDefaultImports
 import uy.kohesive.keplin.util.KotlinScriptDefinitionEx
 import java.io.File
 import java.util.concurrent.Future
 import kotlin.reflect.KClass
-import kotlin.reflect.KParameter
-import kotlin.reflect.full.primaryConstructor
 import kotlin.script.dependencies.*
 import kotlin.script.experimental.dependencies.DependenciesResolver
 import kotlin.script.templates.DEFAULT_SCRIPT_FILE_PATTERN
@@ -36,8 +22,7 @@ open class AnnotationTriggeredScriptDefinition(definitionName: String,
                                                defaultEmptyArgs: ScriptArgsWithTypes? = null,
                                                val resolvers: List<AnnotationTriggeredScriptResolver> = emptyList(),
                                                defaultImports: List<String> = emptyList(),
-                                               val scriptFilePattern: Regex = DEFAULT_SCRIPT_FILE_PATTERN.toRegex(),
-                                               val environment: Map<String, Any?>? = null) :
+                                               val scriptFilePattern: Regex = DEFAULT_SCRIPT_FILE_PATTERN.toRegex()) :
         KotlinScriptDefinitionEx(template, defaultEmptyArgs, defaultImports) {
     override val name = definitionName
     override val acceptedAnnotations = resolvers.map { it.acceptedAnnotations }.flatten()
@@ -57,76 +42,26 @@ open class AnnotationTriggeredScriptDefinition(definitionName: String,
         override val scripts: Iterable<File> get() = (current.scripts + backup.scripts).distinct()
     }
 
-    override val dependencyResolver: DependenciesResolver = ApiChangeDependencyResolverWrapper(OldSchoolDependencyResolver())
+    override val dependencyResolver: DependenciesResolver = ApiChangeDependencyResolverWrapper(
+            OldSchoolDependencyResolver(defaultImports)
+    )
 
-
-    private inner class OldSchoolDependencyResolver : ScriptDependenciesResolver {
+    private inner class OldSchoolDependencyResolver(val defaultImports: List<String>) : ScriptDependenciesResolver {
         override fun resolve(script: ScriptContents, environment: Environment?, report: (ScriptDependenciesResolver.ReportSeverity, String, ScriptContents.Position?) -> Unit, previousDependencies: KotlinScriptExternalDependencies?): Future<KotlinScriptExternalDependencies?> {
             fun logClassloadingError(ex: Throwable) {
                 logScriptDefMessage(ScriptDependenciesResolver.ReportSeverity.WARNING, ex.message ?: "Invalid script template: ${template.qualifiedName}", null)
             }
 
             try {
-                val fileDeps = resolutionManager.resolve(script, environment, Companion::logScriptDefMessage, previousDependencies)
-
-                val updatedDependencies = fileDeps.get()
-                val backupDependencies = super.resolve(script, environment, report, previousDependencies).get()
-                return if (updatedDependencies == null || backupDependencies == null) PseudoFuture(updatedDependencies ?: backupDependencies)
+                val updatedDependencies = resolutionManager.resolve(script, environment, Companion::logScriptDefMessage, previousDependencies).get()
+                val backupDependencies = EmptyDependenciesWithDefaultImports(defaultImports, EmptyDependencies())
+                return if (updatedDependencies == null) PseudoFuture(backupDependencies)
                 else PseudoFuture(MergeDependencies(updatedDependencies, backupDependencies))
             } catch (ex: Throwable) {
                 logClassloadingError(ex)
             }
             return PseudoFuture(null)
         }
-
-        private val KtAnnotationEntry.typeName: String get() = (typeReference?.typeElement as? KtUserType)?.referencedName.orAnonymous()
-
-        internal fun String?.orAnonymous(kind: String = ""): String =
-                this ?: "<anonymous" + (if (kind.isNotBlank()) " $kind" else "") + ">"
-
-        internal fun constructAnnotation(psi: KtAnnotationEntry, targetClass: KClass<out Annotation>): Annotation {
-
-            val valueArguments = psi.valueArguments.map { arg ->
-                val evaluator = ConstantExpressionEvaluator(DefaultBuiltIns.Instance, LanguageVersionSettingsImpl.DEFAULT)
-                val trace = BindingTraceContext()
-                val result = evaluator.evaluateToConstantValue(arg.getArgumentExpression()!!, trace, TypeUtils.NO_EXPECTED_TYPE)
-                // TODO: consider inspecting `trace` to find diagnostics reported during the computation (such as division by zero, integer overflow, invalid annotation parameters etc.)
-                val argName = arg.getArgumentName()?.asName?.toString()
-                argName to result?.value
-            }
-            val mappedArguments: Map<KParameter, Any?> =
-                    tryCreateCallableMappingFromNamedArgs(targetClass.constructors.first(), valueArguments)
-                            ?: return InvalidScriptResolverAnnotation(psi.typeName, valueArguments)
-
-            try {
-                return targetClass.primaryConstructor!!.callBy(mappedArguments)
-            } catch (ex: Exception) {
-                return InvalidScriptResolverAnnotation(psi.typeName, valueArguments, ex)
-            }
-        }
-
-
-        private fun <TF : Any> getAnnotationEntries(file: TF, project: Project): Iterable<KtAnnotationEntry> = when (file) {
-            is PsiFile -> getAnnotationEntriesFromPsiFile(file)
-            is VirtualFile -> getAnnotationEntriesFromVirtualFile(file, project)
-            is File -> {
-                val virtualFile = (StandardFileSystems.local().findFileByPath(file.canonicalPath)
-                        ?: throw IllegalArgumentException("Unable to find file ${file.canonicalPath}"))
-                getAnnotationEntriesFromVirtualFile(virtualFile, project)
-            }
-            else -> throw IllegalArgumentException("Unsupported file type $file")
-        }
-
-        private fun getAnnotationEntriesFromPsiFile(file: PsiFile) =
-                (file as? KtFile)?.annotationEntries
-                        ?: throw IllegalArgumentException("Unable to extract kotlin annotations from ${file.name} (${file.fileType})")
-
-        private fun getAnnotationEntriesFromVirtualFile(file: VirtualFile, project: Project): Iterable<KtAnnotationEntry> {
-            val psiFile: PsiFile = PsiManager.getInstance(project).findFile(file)
-                    ?: throw IllegalArgumentException("Unable to load PSI from ${file.canonicalPath}")
-            return getAnnotationEntriesFromPsiFile(psiFile)
-        }
-
     }
 
     class InvalidScriptResolverAnnotation(val name: String, val annParams: List<Pair<String?, Any?>>?, val error: Exception? = null) : Annotation
--- keplin-util/src/main/kotlin/uy/kohesive/keplin/util/ScriptDefinition.kt
+++ keplin-util/src/main/kotlin/uy/kohesive/keplin/util/ScriptDefinition.kt
@@ -21,23 +21,28 @@ open class KotlinScriptDefinitionEx(template: KClass<out Any>,
                                     override val defaultEmptyArgs: ScriptArgsWithTypes?,
                                     val defaultImports: List<String> = emptyList())
     : KotlinScriptDefinition(template), ScriptTemplateEmptyArgsProvider {
-    class EmptyDependencies() : KotlinScriptExternalDependencies
-    class DefaultImports(val defaultImports: List<String>, val base: KotlinScriptExternalDependencies) : KotlinScriptExternalDependencies by base {
-        override val imports: List<String> get() = (defaultImports + base.imports).distinct()
-    }
 
     override val dependencyResolver: DependenciesResolver
-        get() = super.dependencyResolver
+            = ApiChangeDependencyResolverWrapper(DefaultImportsDependencyResolver(defaultImports))
+}
 
-    private inner class OldSchoolDependencyResolver : ScriptDependenciesResolver {
-        override fun resolve(script: ScriptContents, environment: Environment?, report: (ScriptDependenciesResolver.ReportSeverity, String, ScriptContents.Position?) -> Unit, previousDependencies: KotlinScriptExternalDependencies?): Future<KotlinScriptExternalDependencies?> {
-            val base = super.resolve(script, environment, report, previousDependencies).get()
-            val result = if (previousDependencies == null && defaultImports.isNotEmpty()) DefaultImports(defaultImports, base ?: EmptyDependencies())
-            else base
-            return PseudoFuture(result)
+class DefaultImportsDependencyResolver(val defaultImports: List<String>) : ScriptDependenciesResolver {
+    override fun resolve(script: ScriptContents, environment: Environment?, report: (ScriptDependenciesResolver.ReportSeverity, String, ScriptContents.Position?) -> Unit, previousDependencies: KotlinScriptExternalDependencies?): Future<KotlinScriptExternalDependencies?> {
+        val base = super.resolve(script, environment, report, previousDependencies).get()
+        val result = if (previousDependencies == null && defaultImports.isNotEmpty()) {
+            EmptyDependenciesWithDefaultImports(defaultImports, base ?: EmptyDependencies())
+        } else {
+            base
         }
+        return PseudoFuture(result)
     }
 }
+
+class EmptyDependencies() : KotlinScriptExternalDependencies
+class EmptyDependenciesWithDefaultImports(val defaultImports: List<String>, val base: KotlinScriptExternalDependencies) : KotlinScriptExternalDependencies by base {
+    override val imports: List<String> get() = (defaultImports + base.imports).distinct()
+}
+
 
 class ApiChangeDependencyResolverWrapper(
         override val delegate: kotlin.script.dependencies.ScriptDependenciesResolver
