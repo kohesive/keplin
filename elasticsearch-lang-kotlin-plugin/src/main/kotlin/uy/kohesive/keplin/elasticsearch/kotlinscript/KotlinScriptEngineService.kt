5d3fdc99d1c0bd733f9071adc5d13807447a622e
apatrida <jayson.minard@gmail.com>
2017-11-19 14:38:20 -0200

    remove dead comments

--- elasticsearch-lang-kotlin-plugin/src/main/kotlin/uy/kohesive/keplin/elasticsearch/kotlinscript/KotlinScriptEngineService.kt
+++ elasticsearch-lang-kotlin-plugin/src/main/kotlin/uy/kohesive/keplin/elasticsearch/kotlinscript/KotlinScriptEngineService.kt
@@ -35,30 +35,6 @@ import java.security.PrivilegedAction
 import java.util.*
 import java.util.concurrent.atomic.AtomicInteger
 
-/*
-interface ScriptTemplateEmptyArgsProvider {
-    val defaultEmptyArgs: ScriptArgsWithTypes?
-}
-
-open class KotlinScriptDefinitionEx(template: KClass<out Any>,
-                                    override val defaultEmptyArgs: ScriptArgsWithTypes?,
-                                    val defaultImports: List<String> = emptyList())
-    : KotlinScriptDefinition(template), ScriptTemplateEmptyArgsProvider {
-    class EmptyDependencies() : KotlinScriptExternalDependencies
-    class DefaultImports(val defaultImports: List<String>, val base: KotlinScriptExternalDependencies) : KotlinScriptExternalDependencies by base {
-        override val imports: List<String> get() = (defaultImports + base.imports).distinct()
-    }
-
-    /* TODO: fix this dependency stuff
-    override fun <TF : Any> getDependenciesFor(file: TF, project: Project, previousDependencies: KotlinScriptExternalDependencies?): KotlinScriptExternalDependencies? {
-        val base = super.getDependenciesFor(file, project, previousDependencies)
-        return if (previousDependencies == null && defaultImports.isNotEmpty()) DefaultImports(defaultImports, base ?: EmptyDependencies())
-        else base
-    }
-    */
-}
-*/
-
 class KotlinScriptEngineService(val settings: Settings) : ScriptEngineService {
     companion object {
         val LANGUAGE_NAME = KotlinScriptPlugin.LANGUAGE_NAME
