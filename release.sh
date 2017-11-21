33e4c1652e42487904c76f235e87f9c04358a575
apatrida <jayson.minard@gmail.com>
2017-11-20 22:07:00 -0200

    don't double run tests on release

--- release.sh
+++ release.sh
@@ -2,6 +2,6 @@ set -e
 ./gradlew --stop
 ./gradlew clean build check
 set +e
-./gradlew binTray --no-daemon  --max-workers=1
-./gradlew uploadArchives closeAndReleaseRepository --no-daemon  --max-workers=1
+./gradlew -x check -x test binTray --no-daemon  --max-workers=1
+./gradlew -x check -x test uploadArchives closeAndReleaseRepository --no-daemon  --max-workers=1
 
